f4c043e52b25476eb1bd301a15f75a24aa6028a0
elihaimov1992 <37331405+elihaimov1992@users.noreply.github.com>
2019-05-22 15:34:18 +0300

    Update Query.cpp

--- Query.cpp
+++ Query.cpp
@@ -17,59 +17,59 @@ std::shared_ptr<QueryBase> QueryBase::factory(const string& s)
   // Example: if(s == "smart") return std::shared_ptr<QueryBase>(new WordQuery("smart"));
 
   // All Regex:
-  regex regex_and(R"(^\\s+(\\w+)\\s+AND\\s+(\\w+)\\s*$)");
-  regex regex_or(R"(^\\s+(\\w+)\\s+OR\\s+(\\w+)\\s*$)");
-  regex regex_n(R"(^\\s+(\\w+)\\s+\(d+)\\s+(\\w+)\\s*$)");
-  regex regex_not(R"(^\\s*NOT\\s+(\\w+)\\s*$)");
-  regex regex_word((R"(^\\s*(\\w+)\\s*$)"));
+  regex regex_and("^\\s+([\\w']+)\\s+(AND)\\s+([\\w']+)\\s*$");
+  regex regex_or("^\\s+([\\w']+)\\s+(OR)\\s+([\\w']+)\\s*$");
+  regex regex_n("^\\s+([\\w']+)\\s+(\\d+)\\s+([\\w']+)\\s*$");
+  regex regex_not("^\\s*NOT\\s+([\\w']+)\\s*$");
+  regex regex_word("^\\s*([\\w']+)\\s*$");
+
+
 
   // AND case:
-  auto words_begin = sregex_iterator(s.begin(), s.end(), regex_and);
-  auto words_end = sregex_iterator();
-  smatch Match = *words_begin;
-  if(distance(words_begin,words_end) > 0)
+  // source: www.stackoverflow.com/questions/5607589/right-way-to-split-an-stdstring-into-a-vectorstring
+  if (regex_match(s, regex_and)) 
   {
-    return std::shared_ptr<QueryBase>(new AndQuery(Match.str(1),Match.str(2)));
-  }
+		istringstream iss(s);
+		vector<string> res((istream_iterator<string>(iss)),istream_iterator<string>());
+		return shared_ptr<QueryBase>(new AndQuery(res[0], res[2]));
+	}
 
   // OR case:
-  auto words_begin = sregex_iterator(s.begin(), s.end(), regex_or);
-  auto words_end = sregex_iterator();
-  smatch Match = *words_begin;
-  if(distance(words_begin,words_end) > 0)
+  else if (regex_match(s, regex_or)) 
   {
-    return std::shared_ptr<QueryBase>(new OrQuery(Match.str(1),Match.str(2)));
-  }
+		istringstream iss(s);
+		vector<string> res((istream_iterator<string>(iss)),istream_iterator<string>());
+		return shared_ptr<QueryBase>(new OrQuery(res[0], res[2]));
+	}
 
   // n case:
-  auto words_begin = sregex_iterator(s.begin(), s.end(), regex_n);
-  auto words_end = sregex_iterator();
-  smatch Match = *words_begin;
-  if(distance(words_begin,words_end) > 0)
+  else if (regex_match(s, regex_n)) 
   {
-    return std::shared_ptr<QueryBase>(new NQuery(Match.str(1),Match.str(3),stoi(Match.str(2))));
-  }
+		istringstream iss(s);
+		vector<string> res((istream_iterator<string>(iss)),istream_iterator<string>());
+		return shared_ptr<QueryBase>(new NQuery(res[0], res[2],stoi(res[1])));
+	}
 
   // NOT case:
-  auto words_begin = sregex_iterator(s.begin(), s.end(), regex_not);
-  auto words_end = sregex_iterator();
-  smatch Match = *words_begin;
-  if(distance(words_begin,words_end) > 0)
+  else if (regex_match(s, regex_not)) 
   {
-    return std::shared_ptr<QueryBase>(new NotQuery(Match.str(1)));
-  }
+		istringstream iss(s);
+		vector<string> res((istream_iterator<string>(iss)),istream_iterator<string>());
+		return shared_ptr<QueryBase>(new NotQuery(res[1]));
+	}
 
   // word case:
-  auto words_begin = sregex_iterator(s.begin(), s.end(), regex_word);
-  auto words_end = sregex_iterator();
-  smatch Match = *words_begin;
-  if(distance(words_begin,words_end) > 0)
+  else if (regex_match(s, regex_word)) 
   {
-    return std::shared_ptr<QueryBase>(new WordQuery(Match.str(1)));
-  }
+		istringstream iss(s);
+		vector<string> res((istream_iterator<string>(iss)),istream_iterator<string>());
+		return shared_ptr<QueryBase>(new WordQuery(s));
+	}
 
-  // else
-  cout << "Unrecognized search" << endl;
+  else
+  {
+    throw invalid_argument("Unrecognized search");
+  }
 
 }
 ////////////////////////////////////////////////////////////////////////////////
@@ -122,13 +122,14 @@ QueryResult NQuery::eval(const TextQuery &text) const
 
 QueryResult res = AndQuery::eval(text); 
 auto ret_lines = std::make_shared<std::set<line_no>>();
+auto it = res.begin();
 
 // All Regex:
 regex regex_words("[\\w']+");
 regex regex_R2L("(.)*" + right_query + " ([\\w']+ ){0," + to_string(dist) +"}" + left_query + "(.)*");
 regex regex_L2R("(.)*" + left_query + " ([\\w']+ ){0," + to_string(dist) +"}" + right_query + "(.)*");
 
-for (auto it = res.begin(); it != res.end(); ++it) {
+for (; it != res.end(); ++it) {
 		string line = res.get_file()->at(*it);
 		string new_line = "";
 		auto start = sregex_iterator(line.begin(), line.end(), regex_words);
@@ -138,7 +139,8 @@ for (auto it = res.begin(); it != res.end(); ++it) {
 			string Match_str = Match.str();
 			new_line = new_line + Match_str + " ";
 		}
-		if (regex_match(new_line, regex_R2L) || regex_match(new_line, regex_L2R)) {
+		if (regex_match(new_line, regex_R2L) || regex_match(new_line, regex_L2R)) 
+    {
 			ret_lines->insert(*it);
 		}
 	}
